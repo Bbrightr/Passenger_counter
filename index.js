1b6863ead8a2b62f6d28c382d57f4d9f7ea3c4a4
Bright Obiri Gyan <74192474+Bbrightr@users.noreply.github.com>
2025-02-17 00:15:53 +0100

    New update 1.0

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 let countEl = document.getElementById("count-el")
 let saveEl = document.getElementById("save-el")
 let count = 0
+let newState = 0
 let clear = 0
 
 function increment() {
@@ -10,18 +11,16 @@ function increment() {
 }
 
 function save() {
-    newState = count + "-"
-    // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
-    // 3. Render the variable in the saveEl using innerText
-    saveEl.innerText = newState + count
-    // newState = newState * 0
-    // NB: Make sure to not delete the existing content of the paragraph
-    console.log(count)
+    let newState = count + " - "
+    saveEl.innerText += newState
+    count = 0
+    countEl.innerText = 0
 }
 
 function clearState(){
-    clear = count*0
-    countEl.innerText = clear
+    countEl.innerText = 0
+    saveEl.innerText = 0
+
 }
 
 
